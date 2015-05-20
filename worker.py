7060f48a2bbe71a54c1b3d893a8f5f322de089b6
maciek <tapczan666@gmail.com>
2015-05-20 15:59:02 +0200

    More welch method work.

--- worker.py
+++ worker.py
@@ -37,43 +37,50 @@ class Worker(QtCore.QObject):
         samples = samples - np.mean(samples)
         #power, freqs = psd(samples, NFFT=nfft, pad_to=length, noverlap=nfft/2, Fs=sampRate/1e6, detrend=mlab.detrend_mean, window=mlab.window_hanning, sides = 'twosided')
         power, freqs = self.welch(samples, nfft, length, sampRate/1e6)
-        #power = np.reshape(power, len(power))
+        power = np.reshape(power, len(power))
 
         freqs = freqs + center_freq/1e6
         power = power[trash/2:-trash/2]
         freqs = freqs[trash/2:-trash/2]
 
         power = 10*np.log10(power)
-        power = power - self.correction
+        #power = power - self.correction
         out = [index, power, freqs]
         self.dataReady.emit(out)
 
     def welch(self, x, nfft, pad_to, sampRate):
         window = mlab.window_hanning
+        x = np.asarray(x)
+
+        numFreqs = pad_to
+        if pad_to % 2:
+            freqcenter = (pad_to - 1)//2 + 1
+        else:
+            freqcenter = pad_to//2
 
         # Split input vector into slices
-        temp = stride_windows(x, nfft, nfft/2, axis=1)
+        temp = stride_windows(x, nfft, nfft/2, axis=0)
 
         # Apply window function
-        temp, windowVal = apply_window(temp, window, axis=1, return_window=True)
+        temp, windowVal = apply_window(temp, window, axis=0, return_window=True)
 
         # Calculate window normalization
         S_1 = (np.abs(windowVal)).sum()
 
         # Calculate FFT
-        power = np.fft.fft(temp, pad_to)
+        power = np.fft.fft(temp, pad_to, axis=0)[:numFreqs, :]
 
-        freqs = np.fft.fftfreq(pad_to, 1/sampRate)
-        power = np.conjugate(power)*power
+        freqs = np.fft.fftfreq(pad_to, 1/sampRate)[:numFreqs]
+
+        power = np.conjugate(power) * power
         power /= S_1**2
-        power = power.real
 
-        freqcenter = pad_to/2
         freqs = np.concatenate((freqs[freqcenter:], freqs[:freqcenter]))
-        power = np.concatenate((power[freqcenter:, :],
-                                 power[:freqcenter, :]), 0)
+        power = np.concatenate((power[freqcenter:, :], power[:freqcenter, :]), 0)
+
         # Average the power spectra
-        power = np.mean(power, axis=0)
+        power = np.mean(power, axis=1)
+        power = power.real
 
         return power, freqs
 
